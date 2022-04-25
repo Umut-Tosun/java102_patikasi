fb6e1f0ddfeca0d60d8cb2e1bd4bfd9313816dde
Umut Tosun <tosun.umutt@gmail.com>
2022-04-25 21:03:18 +0300

    Encapsulation örnekleri eklendi.

---
+++ src/ObjectOrientedProgramming/Book.java
@@ -0,0 +1,31 @@
+package ObjectOrientedProgramming;
+
+public class Book {
+    private String author,bookName;
+    private int numberOfPages;
+
+    public Book(String author, String bookName, int numberOfPages) {
+        this.author = author;
+        this.bookName = bookName;
+        if (numberOfPages<0)
+            this.numberOfPages=0;
+        else this.numberOfPages = numberOfPages;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        if (numberOfPages<0)
+            this.numberOfPages=0;
+        else this.numberOfPages = numberOfPages;
+    }
+    public void printBook()
+    {
+        System.out.println("Kitap İsmi : "+bookName);
+        System.out.println("Yazar : "+author);
+        System.out.println("Sayfa Sayısı : "+numberOfPages);
+        System.out.println("================================");
+    }
+}
---
+++ src/ObjectOrientedProgramming/BookMain.java
@@ -0,0 +1,19 @@
+package ObjectOrientedProgramming;
+
+public class BookMain {
+    public static void main(String[] args) {
+        System.out.println("================================");
+
+        Book nBook = new Book("Ahmet Ümit","İstanbul Hatıraıs",567);
+        nBook.printBook();
+        nBook.setNumberOfPages(-568);
+        nBook.printBook();
+
+        Book nBookTwo = new Book("Ahmet Ümit","Kırlangıç Çığlığı",600);
+        nBookTwo.printBook();
+        nBookTwo.setNumberOfPages(-1);
+        nBookTwo.printBook();
+
+
+    }
+}
