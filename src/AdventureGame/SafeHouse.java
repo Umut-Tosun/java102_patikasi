8c869d6ba64d7b6df9862b2f9bdcf1676475a4bd
Umut Tosun <tosun.umutt@gmail.com>
2022-05-21 17:38:45 +0300

    Oyunu kazanma senaryosu eklendi.

--- src/AdventureGame/SafeHouse.java
+++ src/AdventureGame/SafeHouse.java
@@ -1,5 +1,7 @@
 package AdventureGame;
 
+import java.util.Arrays;
+
 public class SafeHouse extends NormalLocation{
     public SafeHouse(Player player) {
         super(player, "Güvenli Ev",1,"Savaştan yorulduysan Aradığın Yer Burası");
@@ -8,6 +10,16 @@ public class SafeHouse extends NormalLocation{
     public Boolean onLocation() {
         System.out.println("\nGüvenli Evdesin ! Canın Yenilendi");
         this.getPlayer().setHealth(this.getPlayer().getDefHealth());
+        if (Game.territoriesWon.contains("Mağara")&&Game.territoriesWon.contains("Maden")&&Game.territoriesWon.contains("Nehir")&&Game.territoriesWon.contains("Orman"))
+        {
+            System.out.println();
+            System.out.println(ANSI_CYAN+this.getPlayer().getName()+ANSI_YEŞİL+" Tebrikler ! Oyunu Başarıyla Tamamladın");
+            System.out.println();
+            System.out.println(ANSI_KIRMIZI+"Zanka'da "+ANSI_YEŞİL+" Tüm Bölgelerdeki Düşmanları Temizledin ! "+ANSI_RESET);
+            System.out.println();
+            System.out.println(ANSI_CYAN+"Kanlar içinde kalan düşmanların arasından yorgun bir şekilde geçiyorsun ! Yeni Bir Macerada Görüşmek Üzere");
+            System.exit(0);
+        }
         return true;
     }
 }
