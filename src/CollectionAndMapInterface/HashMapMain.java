41b209ad858aec757926bdec97f87d66478d3501
Umut Tosun <tosun.umutt@gmail.com>
2022-06-07 14:56:42 +0300

    HashMap Sınıfı  incelendi.

---
+++ src/CollectionAndMapInterface/HashMapMain.java
@@ -0,0 +1,32 @@
+package CollectionAndMapInterface;
+import java.util.HashMap;
+
+public class HashMapMain {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        capitalCities.put("Turkey", "Istanbul");
+        System.out.println(capitalCities);
+
+        capitalCities.get("England");
+        capitalCities.remove("England");
+        capitalCities.clear();
+        capitalCities.size();
+
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+    }
+}
